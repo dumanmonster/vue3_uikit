724ee7c2990ba64b72b279409d0f7a164a194e7e
dumanmonster <dybysbayd@gmail.com>
2023-02-20 23:28:36 +0600

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup>
+import LayoutHeader from "@/components/layout/Header.vue";
+</script>
 
 <template>
-  <router-view />
+  <div class="container">
+    <layout-header />
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@import "./styles/global.scss";
+#app {
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+.content {
+  max-width: 1400px;
+  margin-left: 250px;
+  padding: 30px;
+  transition: 0.2s;
+  &._full {
+    margin-left: 0;
+  }
+}
+</style>
