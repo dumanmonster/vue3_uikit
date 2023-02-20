13ad3d697ccfb59de75fd78fa86f12c1c3e295be
dumanmonster <dybysbayd@gmail.com>
2023-02-21 00:57:20 +0600

    checkbox view start

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Index from "@/views/Index.vue";
 import Button from "@/views/Button.vue";
 import Typography from "@/views/Typography.vue";
+import Checkbox from "@/views/Checkbox.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -15,11 +16,15 @@ const router = createRouter({
       name: "Typography",
       component: Typography,
     },
-
     {
       path: "/button",
       name: "Button",
       component: Button,
+    },
+    {
+      path: "/checkbox",
+      name: "Checkbox",
+      component: Checkbox,
     },
   ],
 });
