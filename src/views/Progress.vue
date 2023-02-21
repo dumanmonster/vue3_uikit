3d403629bad56eeb27d02664ec59a92e2d274f49
dumanmonster <dybysbayd@gmail.com>
2023-02-22 00:11:53 +0600

    removing percent in progress

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -11,10 +11,22 @@ const addPercentBar = () => {
     percentBar.value = 100;
   }
 };
+const removePercentBar = () => {
+  percentBar.value -= 10;
+  if (percentBar.value < 0) {
+    percentBar.value = 0;
+  }
+};
 const addPercentCircle = () => {
   percentCircle.value += 10;
   if (percentCircle.value > 100) {
     percentCircle.value = 100;
+  }
+};
+const removePercentCircle = () => {
+  percentCircle.value -= 10;
+  if (percentCircle.value < 0) {
+    percentCircle.value = 0;
   }
 };
 </script>
@@ -24,10 +36,12 @@ const addPercentCircle = () => {
   <div class="line line_block">
     <progress-bar maxWidth="400px" :percent="percentBar" color="danger" />
     <Button label="Add 10%" @click="addPercentBar" />
+    <Button label="Remove 10%" @click="removePercentBar" />
   </div>
   <h2 class="heading-2">Progress Circle</h2>
   <div class="line line_block">
     <ProgressCircle :percent="percentCircle" color="warning" />
     <Button label="Add 10%" @click="addPercentCircle" />
+    <Button label="Remove 10%" @click="removePercentCircle" />
   </div>
 </template>
