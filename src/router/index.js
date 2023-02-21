618175844f96bc05a11cb69796a4bbb859790da5
dumanmonster <dybysbayd@gmail.com>
2023-02-22 00:03:38 +0600

    Radio Button View

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Button from "@/views/Button.vue";
 import Typography from "@/views/Typography.vue";
 import Checkbox from "@/views/Checkbox.vue";
 import RadioButton from "@/views/RadioButton.vue";
+import Progress from "@/views/Progress.vue";
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -31,6 +33,11 @@ const router = createRouter({
       path: "/radio-button",
       name: "RadioButton",
       component: RadioButton,
+    },
+    {
+      path: "/progress",
+      name: "Progress",
+      component: Progress,
     },
   ],
 });
--- src/views/RadioButton.vue
+++ src/views/RadioButton.vue
@@ -1,6 +1,41 @@
-<script setup> 
+<script setup>
+import { ref } from "vue";
+import RadioButton from "@/components/RadioButton.vue";
+const footballClubs = ref([
+  { name: "CSKA Moscow", id: "f1" },
+  { name: "Spartak Moscow", id: "f2" },
+  { name: "Real Madrid", id: "f3" },
+  { name: "Milan", id: "f4" },
+]);
+const selectedClub = ref("");
+const disabledRadio = ref(true);
+const disabledRadioChecked = ref(true);
 </script>
 
 <template>
-    ddsd
+  <h1 class="heading-1">Radiobutton</h1>
+  <h2 class="heading-2">Basic</h2>
+  <div class="line line_block">
+    <p>Selecled Club: {{ selectedClub }}</p>
+    <div v-for="club in footballClubs" :key="club.id">
+      <radio-button
+        :value="club.name"
+        :label="club.name"
+        :id="club.id"
+        name="club"
+        v-model:checkedValue="selectedClub"
+      />
+    </div>
+  </div>
+  <h2 class="heading-2">Disabled</h2>
+  <div class="line">
+    <radio-button
+      value="Radiobutton Disabled"
+      label="Radiobutton Disabled"
+      id="RadiobuttonDisabled"
+      name="RadiobuttonDisabled"
+      :disabled="disabledRadio"
+      v-model:checkedValue="disabledRadioChecked"
+    />
+  </div>
 </template>
